90a906b207a5d0ce4c50300ca0c6dd0c0f86216b
Xinlana1 <julianaoaguilar16@gmail.com>
2024-09-11 17:36:07 -0300

    Closes #3 - botão somar

---
+++ Projects/BootCamp DevJr/módulo 02/calculadora/calculator.js
@@ -0,0 +1,8 @@
+function calc(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+
+    var result = num1 + num2;
+
+    document.getElementById("result").value = result;
+}
