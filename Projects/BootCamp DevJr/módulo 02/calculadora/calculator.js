5ea0bfcc6ba7ab93fa7de76a249e7da8a7d5a8b0
Xinlana1 <julianaoaguilar16@gmail.com>
2024-09-11 20:15:33 -0300

    Close #5 - histórico

--- Projects/BootCamp DevJr/módulo 02/calculadora/calculator.js
+++ Projects/BootCamp DevJr/módulo 02/calculadora/calculator.js
@@ -11,10 +11,19 @@ case '+' : result = num1 + num2; break;
 case '-' : result = num1 - num2; break;
 case '*' : result = num1 * num2; break;
 case '/' : result = num1 / num2; break;
+case (num1 && num2 == undefined): var result = "Insira os valores"; break;
 
-       
     }
 
+   
     document.getElementById("result").value = result;
-}
+var newHistory = "<div>" + num1 + " " + op + " " + num2 + " = " + result + "</div>";
+var history =  document.getElementById("history");
+   history.innerHTML =  newHistory + history.innerHTML ;
 
+   if(history.children.length > 10)
+   {
+    history.removeChild(history.childNodes[10]);
+   }
+
+}
